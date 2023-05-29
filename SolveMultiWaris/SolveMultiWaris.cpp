ef94d9549e9cff128e46471addd8df18316d8725
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-29 15:42:36 +0700

    class budi inheritance pekerja dan pelajar

--- SolveMultiWaris/SolveMultiWaris.cpp
+++ SolveMultiWaris/SolveMultiWaris.cpp
@@ -12,6 +12,37 @@ public :
     }
 };
 
+class pekerja : virtual public orang {
+public :
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "Pekerja Dibuat\n" << endl;
+    }
+};
+
+class pelajar : virtual public orang {
+public :
+
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "Pelajar Dibuat\n" << endl;
+    }
+};
+
+class budi : public pekerja, public pelajar {
+public :
+
+    budi(int pUmur) :
+        pekerja(pUmur),
+        pelajar(pUmur),
+        orang(pUmur)            //Hal ini dapat digunakan jika menggunakan virtual
+    {
+        cout << "Budi Dibuat\n" << endl;
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
