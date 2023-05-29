12734db2d848213337a0c2d2a616aeae02f19ef4
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-29 14:47:36 +0700

    class orang inheritance

--- multipewarisan/multipewarisan.cpp
+++ multipewarisan/multipewarisan.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public :
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "Orang Dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "Orang Dihapus\n" << endl;
+    }
+};
 
 int main()
 {
